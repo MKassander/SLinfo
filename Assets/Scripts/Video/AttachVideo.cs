6e81bb02ad04536cce47a3b0eab958e70c6adaf9
Max Kassander <maximilian.kassander@gmail.com>
2021-05-11 15:01:41 +0200

    Loop fixat (förhoppningsvis)

--- Assets/Scripts/Video/AttachVideo.cs
+++ Assets/Scripts/Video/AttachVideo.cs
@@ -6,24 +6,9 @@ public class AttachVideo : MonoBehaviour
 {
     private VideoPlayer VideoPlayer => GetComponent<VideoPlayer>();
     public string path;
-    private bool hasPlayed = false;
 
     public void Awake()
     {
         VideoPlayer.url = Path.Combine(Application.streamingAssetsPath, path + ".mp4");
-        VideoPlayer.loopPointReached += VideoPlayerOnloopPointReached;
-    }
-
-    private void VideoPlayerOnloopPointReached(VideoPlayer source)
-    {
-        hasPlayed = true;
-    }
-
-    private void FixedUpdate()
-    {
-        if (!VideoPlayer.isPlaying && !hasPlayed)
-        {
-            VideoPlayer.Play();
-        }
     }
 }
